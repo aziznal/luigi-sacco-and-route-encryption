d40a10bc774ef9ea16a539179ac352567b41eb3e
aziznal <abodenaal@gmail.com>
2021-06-11 08:12:40 +0300

    Updates main-menu.ui and adds placeholder functions for encryption methods

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ from gui import Gui
 import utils
 
 
-if __name__ == '__main__':
-
-    app = QApplication([])
-
+def create_main_window():
+    
     widget_ids = "data/main-ids.json"
     gui_file_path = "data/main-menu.ui"
     kto_image_path = utils.get_path_in_bundle_dir("data/kto_logo.png")
@@ -20,5 +18,28 @@ if __name__ == '__main__':
     gui.get_widget("mainLabel").setPixmap(kto_image)
 
     gui.add_event_listener("exitButton", lambda: app.quit())
+    
+    return gui
+
+
+def create_first_method_window():
+    pass
+
+
+def create_second_method_window():
+    pass
+
+
+if __name__ == '__main__':
+
+    app = QApplication([])
+
+    main_window = create_main_window()
+
+    first_method_window = create_first_method_window()
+    second_method_window = create_second_method_window()
+
+    main_window.add_event_listener("firstMethodButton", lambda: print("Luigi Socco"))
+    main_window.add_event_listener("secondMethodButton", lambda: print("Route Encryption"))
 
     app.exec_()
