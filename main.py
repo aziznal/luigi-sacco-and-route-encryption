693c4c9aa09ed11170ec7971c1f6aa43d64a58f8
aziznal <abodenaal@gmail.com>
2021-06-11 07:10:48 +0300

    Creates Main Menu

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from gui import Gui
 
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QPixmap
 
 
 
@@ -8,25 +9,14 @@ if __name__ == '__main__':
 
     app = QApplication([])
 
-
-    widget_ids = "data/example-ids.json"
-    gui_file_path = "data/example-gui.ui"
+    widget_ids = "data/main-ids.json"
+    gui_file_path = "data/main-menu.ui"
 
     gui = Gui(widget_ids, gui_file_path)
 
-    gui.add_event_listener(
-        widget_id="exitButton",
-        on_event=lambda: exit()
-    )
+    kto_image = QPixmap("data/kto_logo.png")
+    gui.get_widget("mainLabel").setPixmap(kto_image)
 
-
-    def button_onclick():
-        gui.get_widget("mainLabel").setText("Congratulations! You have clicked a button")
-
-    gui.add_event_listener(
-        widget_id="mainButton",
-        on_event=lambda: button_onclick()
-    )
-
+    gui.add_event_listener("exitButton", exit)
 
     app.exec_()
