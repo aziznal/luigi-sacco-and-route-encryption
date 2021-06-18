40f8dd64d8854cb0aa07adfc1b48f9582db74011
aziznal <abodenaal@gmail.com>
2021-06-18 12:52:53 +0300

    Fixes (almost) all issues in encryption and decryption

--- playground/main.py
+++ playground/main.py
@@ -139,16 +139,19 @@ def luigi_sacco_encrypt(key, plain_text, lang="TR", verbose=False):
 
 
 
-def get_indices_of_extra_letters(row, split):
+def get_indices_of_extra_letters(row, split, key_length):
 
     row_without_placeholder = ''.join(char for char in row if char != '_')
     difference = len(row_without_placeholder) - split
     
-    if difference <= 0:
+    difference = key_length - split
+
+    if difference == 0:
         return []
 
     # indices_of_extra_letters = [i for i in range(split, split + difference)]
-    indices_of_extra_letters = [ row.index(letter) for letter in row[split:] if letter!='_' ]
+    indices_of_extra_letters = [ i for i in range(key_length - difference, key_length) ]
+    indices_of_extra_letters = [ e for e in indices_of_extra_letters if row[e] != '_' ]
 
     # print(f"\nExtra indices in {row} are {indices_of_extra_letters}")
 
@@ -158,23 +161,28 @@ def get_indices_of_extra_letters(row, split):
 def recursive_push_down(matrix, row, index_of_letter_to_be_pushed_down):
 
     # If space is empty, then just set character where it needs to go:
-    if matrix[row + 1][index_of_letter_to_be_pushed_down] == '_':
-        matrix[row + 1][index_of_letter_to_be_pushed_down] = matrix[row][index_of_letter_to_be_pushed_down]
-        matrix[row][index_of_letter_to_be_pushed_down] = '_'
-
-    # Else, push the below character to the row below itself
-    else:
+    if matrix[row + 1][index_of_letter_to_be_pushed_down] != '_':
+        # Clear below space
         recursive_push_down(matrix, row + 1, index_of_letter_to_be_pushed_down)
 
+    
+    matrix[row + 1][index_of_letter_to_be_pushed_down] = matrix[row][index_of_letter_to_be_pushed_down]
+    matrix[row][index_of_letter_to_be_pushed_down] = '_'
 
-def push_down(matrix, row, split):
+
+def push_down(matrix, row, split, verbose, key_length):
     # indices_of_extra_letters = [i for i in range(split, len(matrix[row]))]
-    indices_of_extra_letters = get_indices_of_extra_letters(matrix[row], split)
+    indices_of_extra_letters = get_indices_of_extra_letters(matrix[row], split, key_length)
     
     for i in indices_of_extra_letters:
 
         try:
+            # Something's wrong with the recursive algorithm
             recursive_push_down(matrix, row, i)
+
+            if verbose:
+                print(f"\t\t\n\n\nIteration {i}")
+                [print(row) for row in matrix]
 
         except:
             pass
@@ -186,26 +194,29 @@ def super_sophisticated_matrix_transformation_algorithm(matrix, splits, verbose)
 
     # 'PUSH' down any extra characters in the row to the row below it
 
-    # If the row below it has characters filling up the space where we want to push
-    # the extra characters from below, then push them down as well.
+    # If the row below has characters filling up the space where we want to push
+    # the extra characters, then push them down as well.
 
     # Copying matrix to avoid changing the passed in matrix
     matrix = [ [element for element in row] for row in matrix ]
 
     # NOTE: if we had a long message, we'd have more rows than there are splits.
     # and this code would fail. Solution is to divide message into 6 row matrices whenever
-    # is longer than 6 rows, and apply this algorithm on each of those matrices.
+    # message is longer than 6 rows, and apply this algorithm on each of those matrices.
 
     # while len([char for char in matrix[-1] if char != '_']) != splits[-1]:
 
-    for _ in range(10):
+    # There should be a while loop with some condition here
+    for _ in range(100):
         for (i, row), split in zip(enumerate(matrix), splits):
             if len(row) != split:
-                push_down(matrix, i, split)
+                push_down(matrix, i, split, verbose, key_length=len(splits))
+
 
     if verbose:
-        print("\n\n")
+        print("\n\n\n\t\t*** SHAPE OF MATRIX AT THE END ***\n")
         [print(row) for row in matrix]
+        print("\n\n")
 
     return matrix
 
@@ -255,7 +266,7 @@ def luigi_sacco_decrypt(key, encrypted_text, lang="TR", verbose=False):
 
 def test_program(key, plain_text, lang="TR"):
 
-    encrypted_message = luigi_sacco_encrypt(key, plain_text, lang=lang, verbose=True)
+    encrypted_message = luigi_sacco_encrypt(key, plain_text, lang=lang, verbose=False)
     decrypted_message = luigi_sacco_decrypt(key, encrypted_message, lang=lang, verbose=False)
 
 
@@ -272,7 +283,7 @@ def test_program(key, plain_text, lang="TR"):
     print("\n\nDecrypted Message: ")
     print("\n\t" + decrypted_message)
 
-    print("\nHas been decoded correctly?")
+    print("\n\nHas been decoded correctly?")
     if decrypted_message.replace(' ', '') == plain_text.replace(' ', ''):
         print("\n\tYes!")
 
@@ -281,28 +292,49 @@ def test_program(key, plain_text, lang="TR"):
 
 
 
+def execute_all_tests():
+
+    english_keys = [
+        "helloworld",
+        "thisisaverylongkeyindeed",
+        "short",
+        "MuLtiPlEcASe"
+    ]
+
+    english_messages = [
+        "What a wonderful world we live in",
+        "Armies are ready",
+        "Tell everyone that their message should not be too long otherwise the encryption algorithm has trouble with it",
+        "short"
+    ]
+
+    for key in english_keys:
+        for message in english_messages:
+            test_program(key, message, lang="EN")
+
+
 if __name__ == '__main__':
 
     
     # FIXED: if given text is too long, then not all of it is getting encrypted.
-    # FIXME: if key is too long, some weird stuff is happening
-    # FIXME: having repeated letters in a key ruins the decryption process
+    # FIXED: if key is too long, some weird stuff is happening
+    # FIXED: having repeated letters in a key ruins the decryption process
     # TODO: create a big test for encryption and decryption
 
-    # key = "TERAZİ"
-    # plain_text = "ADALET MÜLKÜN TEMELİDİR"
+    key = "TERAZİ"
+    plain_text = "ADALET MÜLKÜN TEMELİDİR"
 
 
     # key = "CONVENIENCE"
     # plain_text = "HERE IS A SECRET MESSAGE ENCIPHERED BY TRANSPOSITION"
 
-    key = "SPANISHINQUISITION"
-    plain_text = "NO ONE EXPECTS THE SPANISH INQUISITION"
+    # key = "SPANISHINQUISITION"
+    # plain_text = "NO ONE EXPECTS THE SPANISH INQUISITION"
 
 
-    # key = "AZİZNALISMYNAME"
+    key = "AZİZNALISMYNAME"
     # key = "AZİZNAL"
-    # plain_text = "BUGÜN ÇOK İYİ BİR GÜN OLACAK"
+    plain_text = "BUGÜN ÇOK İYİ BİR GÜN OLACAK"
 
 
     # test_program(key, plain_text, lang="EN")
