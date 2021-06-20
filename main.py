7ea0e174be404131104a33fffa8c22c85013787d
aziznal <abodenaal@gmail.com>
2021-06-20 21:45:08 +0300

    Adds hooks between luigi sacco gui and functionality

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from PyQt5.QtGui import QPixmap
 
 from gui import Gui
 
+from playground.main import luigi_sacco_encrypt, luigi_sacco_decrypt
+
 import utils
 
 
@@ -81,7 +83,58 @@ def create_second_method_window(main_window):
     return gui
 
 
+
+def run_luigi_sacco(window: Gui):
+
+    get = lambda x: window.get_widget(x)
+
+    language = "EN" if get('englishRadioButton').isChecked() else "TR"
+    key = get('keyTextEdit').toPlainText()
+    plain_text = get('inputTextEdit').toPlainText()
+
+    if key == "" or plain_text == "":
+        return
+
+    encrypt = get('encryptRadioButton')
+    decrypt = get('decryptRadioButton')
+
+    output = ""
+
+    if encrypt.isChecked():
+        output = luigi_sacco_encrypt(key, plain_text, language)
+
+    elif decrypt.isChecked():
+        output = luigi_sacco_decrypt(key, plain_text, language)
+
+
+    get('outputTextEdit').setPlainText(output)
+
+
+def reset_luigi_sacco(window: Gui):
+    window.get_widget('keyTextEdit').clear()
+    window.get_widget('inputTextEdit').clear()
+    window.get_widget('outputTextEdit').clear()
+
+
+
+def add_luigi_sacco_hooks(window: Gui):
+
+    # Check English and Encrypt by default
+    window.get_widget('encryptRadioButton').setChecked(True)
+    window.get_widget('englishRadioButton').setChecked(True)
+
+    # Set listeners for RUN and RESET buttons
+    window.add_event_listener('runButton', lambda: run_luigi_sacco(window))
+    window.add_event_listener('resetButton', lambda: reset_luigi_sacco(window))
+
+    # TODO: add information section about luigi sacco and hook it up here
+
+
 if __name__ == '__main__':
+
+    # TODO
+    #   add error window that can be called on user error and display a
+    #   certain message
 
     app = QApplication([])
 
@@ -103,7 +156,12 @@ if __name__ == '__main__':
 
     main_window.add_event_listener(
         "firstMethodButton", lambda: show_method_window(first_method_window))
+
     main_window.add_event_listener(
         "secondMethodButton", lambda: show_method_window(second_method_window))
+
+
+    # Add hooks to luigi sacco
+    add_luigi_sacco_hooks(first_method_window)
 
     app.exec_()
