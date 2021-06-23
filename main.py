3eb1a5e57bdeb9aecd281452324070078e0a6de6
aziznal <abodenaal@gmail.com>
2021-06-23 23:29:57 +0300

    Reformats and Refactors code

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import PyQt5
-from PyQt5.QtWidgets import QApplication
+from typing import Callable, Literal, Tuple
+
+from PyQt5.QtWidgets import QApplication, QComboBox, QWidget
 from PyQt5.QtGui import QPixmap
+
 
 from gui import Gui
 
@@ -9,15 +11,24 @@ from logic.route_encryption import route_encrypt, route_decrypt, get_potential_t
 
 import utils
 
+ENCRYPT, DECRYPT = (True, False), (False, True)
+
 
 def center_window(window: Gui) -> None:
+    """
+    Center the given window on the screen
+    """
     centered_x = screen_geometry.center().x() - window.width()//2
     centered_y = screen_geometry.center().y() - window.height()//2
 
     window.move(centered_x, centered_y)
 
 
-def create_main_window():
+
+def create_main_window() -> Gui:
+    """
+    Creates app main window from which user can go to luigi sacco or route encryption windows
+    """
 
     widget_ids = "assets/main-ids.json"
     gui_file_path = "assets/main-menu.ui"
@@ -35,7 +46,25 @@ def create_main_window():
     return gui
 
 
-def create_first_method_window(main_window: Gui):
+
+def goto_window(source: Gui, destination: Gui) -> None:
+    """
+    Hides source gui and centers then shows the destination gui
+    """
+    source.hide()
+
+    center_window(destination)
+
+    destination.activateWindow()
+
+    destination.show()
+
+
+
+def create_luigi_sacco_window(main_window: Gui) -> Gui:
+    """
+    Creates a submenu where user can use luigi sacco encryption / decrypytion
+    """
     widget_ids = "assets/first-method-ids.json"
     gui_file_path = "assets/first-method.ui"
 
@@ -43,36 +72,23 @@ def create_first_method_window(main_window: Gui):
 
     gui.hide()
 
-    def show_main_window():
-        gui.hide()
 
-        center_window(main_window)
-
-        main_window.show()
-
-        main_window.activateWindow()
-
-    gui.add_event_listener("backButton", show_main_window)
+    gui.add_event_listener("backButton", lambda: goto_window(gui, main_window))
 
     return gui
 
 
-def create_second_method_window(main_window):
+def create_route_encryption_window(main_window: Gui) -> Gui:
+    """
+    Creates window where user can use route encryption / decryption according to E4 & B3 Routes
+    """
     widget_ids = "assets/second-method-ids.json"
     gui_file_path = "assets/second-method.ui"
 
     gui = Gui(widget_ids, gui_file_path)
 
+    # Make this window hidden by default
     gui.hide()
-
-    def show_main_window():
-        gui.hide()
-
-        center_window(main_window)
-
-        main_window.show()
-
-        main_window.activateWindow()
 
     # Adding images for Route Visualization
     routes_image_path = utils.get_path_in_bundle_dir("assets/routes.png")
@@ -80,103 +96,172 @@ def create_second_method_window(main_window):
 
     gui.get_widget("routesLabel").setPixmap(routes_image)
 
-    gui.add_event_listener("backButton", show_main_window)
+    gui.add_event_listener("backButton", lambda: goto_window(gui, main_window))
 
     return gui
 
 
 
-def run_luigi_sacco(window: Gui):
+def get_luigi_sacco_language(get: Callable[[str], QWidget]) -> Literal["EN", "TR"]:
+    """
+    Extracts and returns chosen language from Luigi Sacco Gui
+    """
+    return "EN" if get('englishRadioButton').isChecked() else "TR"
 
-    get = lambda x: window.get_widget(x)
 
-    language = "EN" if get('englishRadioButton').isChecked() else "TR"
+def get_luigi_sacco_input(get: Callable[[str], QWidget]) -> Tuple[str, str]:
+    """
+    Extracts and returns key and text which user has given in Gui.
+
+    Raises ValueError if either key or text are empty
+    """
+
     key = get('keyTextEdit').toPlainText()
+    
     plain_text = get('inputTextEdit').toPlainText()
 
     if key == "" or plain_text == "":
+        raise ValueError("Key or Plain Text not given")
+    
+    return key, plain_text
+
+
+def get_selected_action(get: Callable[[str], QWidget]) -> Tuple[bool, bool]:
+    """
+    Returns whether given gui is in encrypt or decrypt state.
+    """
+    encrypt = get('encryptRadioButton').isChecked()
+    decrypt = get('decryptRadioButton').isChecked()
+
+    return encrypt, decrypt
+
+
+def run_luigi_sacco(window: Gui) -> None:
+    """
+    Function to run when Run Button is clicked in Luigi Sacco window.
+    
+    Runs luigi sacco encryption / decryption on the given key and
+    input text and takes given language into account.
+
+    Sets the output of the algorithm as the content of the output box
+    """
+
+    # Shortcut for ops in this function
+    get = lambda x: window.get_widget(x)
+
+    language = get_luigi_sacco_language(get)
+
+    try:
+        key, plain_text = get_luigi_sacco_input(get)
+
+    except ValueError:
         return
 
-    encrypt = get('encryptRadioButton')
-    decrypt = get('decryptRadioButton')
+    action = get_selected_action(get)
 
     output = ""
 
-    if encrypt.isChecked():
+    if action == ENCRYPT:
         output = luigi_sacco_encrypt(key, plain_text, language)
 
-    elif decrypt.isChecked():
+    elif action == DECRYPT:
         output = luigi_sacco_decrypt(key, plain_text, language)
-
 
     get('outputTextEdit').setPlainText(output)
 
 
-def reset_luigi_sacco(window: Gui):
+def reset_luigi_sacco(window: Gui) -> None:
+    """
+    Resets gui in luigi sacco to blank state
+    """
     window.get_widget('keyTextEdit').clear()
     window.get_widget('inputTextEdit').clear()
     window.get_widget('outputTextEdit').clear()
 
 
 
-def get_chosen_table_size(input_text, window: Gui):
-    
+def get_chosen_table_size(input_text: str, get: Callable[[str], QWidget]) -> Tuple[int, int]:
+    """
+    Returns chosen table size from gui according to given input text
+    """
+
     if len(input_text) <= 0:
-        return
+        raise (0, 0)
 
     sizes, _ = get_potential_table_sizes(len(input_text))
 
-    return sizes[ window.get_widget('arraySizeComboBox').currentIndex() ]
+    return sizes[get('arraySizeComboBox').currentIndex()]
 
 
-def run_route_encryption(window: Gui):
-
-    get = lambda x: window.get_widget(x)
-
-    # Input Text
+def get_route_encryption_input(get: Callable[[str], QWidget]) -> Tuple[str, Tuple[int, int]]:
+    """
+    Extracts and returns input text and table size from Route Encryption Gui
+    """
     input_text = get('inputTextEdit').toPlainText()
 
-    if len(input_text) <= 0:
+    table_size = get_chosen_table_size(input_text, get)
+
+    return input_text, table_size
+
+
+def run_route_encryption(window: Gui) -> None:
+    """
+    Function to run when Run Button is clicked in Route Encryption window.
+    
+    Runs route encryption / decryption on the given key and
+    input text and takes given language into account.
+
+    Sets the output of the algorithm as the content of the output box
+    """
+
+    # Shortcut for ops in this function
+    def get(x): return window.get_widget(x)
+
+    input_text, table_size = get_route_encryption_input(get)
+
+    if len(input_text) == 0:
         return
 
-    # Table Size
-    table_size = get_chosen_table_size(input_text, window)
-
     # Encrypt vs. Decrypt
-    encrypt = get('encryptRadioButton').isChecked()
-    decrypt = get('decryptRadioButton').isChecked()
+    action = get_selected_action(get)
 
     # Final output message which goes to output box
     output = ""
 
-    if encrypt:
+    if action == ENCRYPT:
         output = route_encrypt(input_text, table_size)
 
-    elif decrypt:
+    elif action == DECRYPT:
         output = route_decrypt(input_text, table_size)
-
 
     get('outputTextEdit').setPlainText(output)
 
 
-def reset_route_encryption(window: Gui):
+def reset_route_encryption(window: Gui) -> None:
+    """
+    Resets route encryption gui to blank state
+    """
 
-    get = lambda x: window.get_widget(x)
+    def get(x): return window.get_widget(x)
 
     get('inputTextEdit').clear()
     get('outputTextEdit').clear()
     get('arraySizeComboBox').clear()
-    
 
-def populate_combobox(combobox, message):
+
+def populate_combobox(combobox: QComboBox, message: str) -> None:
+    """
+    Populates given combobox with potential tables sizes for the given message.
+    """
 
     combobox.clear()
+
     if len(message) <= 0:
         return
 
     # Populate combobox with list of sizes
     sizes, optimal_size = get_potential_table_sizes(len(message))
-    
+
     for size in sizes:
         if size == optimal_size or size == optimal_size[::-1]:
             combobox.addItem(f"{size[0]} x {size[1]} (Recommended)")
@@ -185,8 +270,11 @@ def populate_combobox(combobox, message):
             combobox.addItem(f"{size[0]} x {size[1]}")
 
 
-def add_route_encryption_hooks(window: Gui):
-    get = lambda x: window.get_widget(x)
+def add_route_encryption_hooks(window: Gui) -> None:
+    """
+    Hooks Route Encryption Gui with its Logic
+    """
+    def get(x): return window.get_widget(x)
 
     # Set Encrypt as default option
     get('encryptRadioButton').setChecked(True)
@@ -194,23 +282,37 @@ def add_route_encryption_hooks(window: Gui):
     # Set drop-select to have no elements at the start
     get('arraySizeComboBox').clear()
 
+    input_text = get('inputTextEdit').toPlainText()
+    combobox = get('arraySizeComboBox')
 
     # As text gets typed, the table size combo-box gets filled with new values
     get('inputTextEdit').textChanged.connect(
-        lambda: populate_combobox(get('arraySizeComboBox'), get('inputTextEdit').toPlainText())
+        lambda: populate_combobox(combobox, input_text)
     )
 
     # Run and Reset Button Listeners
-    window.add_event_listener("runButton", lambda: run_route_encryption(window))
-    window.add_event_listener("resetButton", lambda: reset_route_encryption(window))
+    window.add_event_listener(
+        "runButton", lambda: run_route_encryption(window)
+    )
+
+    window.add_event_listener(
+        "resetButton", lambda: reset_route_encryption(window)
+    )
 
 
+def add_luigi_sacco_hooks(window: Gui) -> None:
+    """
+    Hooks Luigi Sacco Gui with its Logic
+    """
 
-def add_luigi_sacco_hooks(window: Gui):
+    # Shortcuts for ops in this function
+    get = lambda x: window.get_widget(x)
 
-    # Check English and Encrypt by default
-    window.get_widget('encryptRadioButton').setChecked(True)
-    window.get_widget('englishRadioButton').setChecked(True)
+    # Check English by default
+    get('englishRadioButton').setChecked(True)
+
+    # Check Encrypt by default
+    get('encryptRadioButton').setChecked(True)
 
     # Set listeners for RUN and RESET buttons
     window.add_event_listener('runButton', lambda: run_luigi_sacco(window))
@@ -219,20 +321,17 @@ def add_luigi_sacco_hooks(window: Gui):
     # TODO: add information section about luigi sacco and hook it up here
 
 
-
 if __name__ == '__main__':
 
     # TODO
     #   add error window that can be called on user error and display a
     #   certain message
 
-    
     # TODO
     #   If message length is prime, either inform user or automatically add
     #   extra letter to make its length non-prime
 
-
-    # TODO: 
+    # TODO:
     #   Make sure to inform user to provide correct table size when
     #   decrypting a message
 
@@ -243,26 +342,21 @@ if __name__ == '__main__':
     SCREEN_HEIGHT = screen_geometry.height()
 
     main_window = create_main_window()
-    first_method_window = create_first_method_window(main_window)
-    second_method_window = create_second_method_window(main_window)
-
-    def show_method_window(method_window):
-        main_window.hide()
-
-        center_window(method_window)
-
-        method_window.show()
-        method_window.activateWindow()
+    luigi_sacco_window = create_luigi_sacco_window(main_window)
+    route_encryption_window = create_route_encryption_window(main_window)
 
     main_window.add_event_listener(
-        "firstMethodButton", lambda: show_method_window(first_method_window))
+        "firstMethodButton",
+        lambda: goto_window(main_window, luigi_sacco_window)
+    )
 
     main_window.add_event_listener(
-        "secondMethodButton", lambda: show_method_window(second_method_window))
+        "secondMethodButton",
+        lambda: goto_window(main_window, route_encryption_window)
+    )
 
+    add_luigi_sacco_hooks(luigi_sacco_window)
 
-    add_luigi_sacco_hooks(first_method_window)
-
-    add_route_encryption_hooks(second_method_window)
+    add_route_encryption_hooks(route_encryption_window)
 
     app.exec_()
--- utils.py
+++ utils.py
@@ -1,33 +1,31 @@
-from typing import Any
+from typing import Any, Dict, List, Union
 
 import sys
 import os
 import json
 
+# This is used to keep paths relative to where the program is unpacked in the temp dir
+bundle_dir: str = getattr(
+    sys,
+    '_MEIPASS',
+    os.path.abspath(os.path.dirname(__file__))
+)
 
-bundle_dir = getattr(sys, '_MEIPASS', os.path.abspath(os.path.dirname(__file__)))
 
-
-def get_path_in_bundle_dir(path):
+def get_path_in_bundle_dir(path: str) -> str:
+    """
+    Returns absolute path equivalent for given path
+    """
     return os.path.abspath(os.path.join(bundle_dir, path))
 
 
-def load_json(file_path: str) -> None:
+def load_json(file_path: str) -> Union[Dict, Dict[str, List[str]]]:
+    """
+    Loads json file at given path and returns it as dict
+    """
 
     file_path = get_path_in_bundle_dir(file_path)
     print(file_path)
 
     with open(file_path) as _file:
         return json.load(_file)
-
-
-def save_as_json(file_path: str, data: Any) -> None:
-
-    raise NotImplementedError()
-
-    file_path = get_path_in_bundle_dir(file_path)
-
-    with open(file_path, 'w') as output_file:
-        json.dump(data, output_file, indent=4)
-
-
